7d9d01db39f72ec91f74019a997fce55b632087c
allanmurimi <allanmurimi@gmail.com>
2021-09-17 17:46:12 +0300

    update html structure classNames

--- order.js
+++ order.js
@@ -17,7 +17,7 @@ if (document.readyState == "loading") {
       input.addEventListener("change", quantityChanged);
     }
   
-    var addToCartButtons = document.getElementsByClassName("shop-item-button");
+    var addToCartButtons = document.getElementsByClassName("pizza-item-button");
     for (var i = 0; i < addToCartButtons.length; i++) {
       var button = addToCartButtons[i];
       button.addEventListener("click", addToCartClicked);
@@ -53,10 +53,10 @@ if (document.readyState == "loading") {
   
   function addToCartClicked(event) {
     var button = event.target;
-    var shopItem = button.parentElement.parentElement;
-    var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
-    var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
-    var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+    var pizzaItem = button.parentElement.parentElement;
+    var title = pizzaItem.getElementsByClassName("pizza-item-title")[0].innerText;
+    var price = pizzaItem.getElementsByClassName("pizza-item-price")[0].innerText;
+    var imageSrc = pizzaItem.getElementsByClassName("pizza-item-image")[0].src;
     addItemToCart(title, price, imageSrc);
     updateCartTotal();
   }
